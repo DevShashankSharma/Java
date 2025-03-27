15056f040d696be2147d776a9e62ea72d862b68e
DevShashankSharma <editorshashank429@gmail.com>
2025-03-27 22:23:04 +0530

    variables

---
+++ 001variables/Variables.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Variables {
+    public static void main (String[] args) {
+        // print function
+        // System.out.println("Hello World"); // print with newline
+        // System.out.print("Hello World"); // print without newline
+        // System.out.printf("Hello %s", "World"); // formatted print
+        // System.out.format("Hello %s", "World"); // formatted print
+        // System.out.print("Hello World \n"); //  print with newline
+
+        // variables
+        // int x = 10;
+        // int y = 20;
+        // int z = x + y;
+        // System.out.println(z);
+
+        // input 
+        Scanner scan = new Scanner(System.in);
+        // System.out.print("Enter Name: "); 
+        // String num = scan.next(); // read string input only until space
+        // System.out.print("Enter your name: ");
+        // String name = scan.nextLine(); // read string input until newline
+        // System.out.print("Enter your age: ");
+        // int age = scan.nextInt(); // read integer input
+        // System.out.print("Enter your height: ");
+        // float height = scan.nextFloat(); // read float input
+        // System.out.print("Enter your weight: ");
+        // double weight = scan.nextDouble(); // read double input
+        // System.out.println("Name: " + name);
+        // System.out.println("Age: " + age);
+        // System.out.println("Height: " + height);
+        // System.out.println("Weight: " + weight);
+
+        scan.close(); // close the scanner
+    }
+}
