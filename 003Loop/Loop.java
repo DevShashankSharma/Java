c59e8904a3059d3f35577cb83d3593b343d34757
DevShashankSharma <editorshashank429@gmail.com>
2025-03-28 07:27:25 +0530

    loops

---
+++ 003Loop/Loop.java
@@ -0,0 +1,63 @@
+
+public class Loop {
+    public static void main(String[] args) {
+        // // for loop
+        // for (int i = 0 ; i < 5 ; i++) {
+        //     System.out.println(i);
+        // }
+
+        // // while loop
+        // int i = 0;
+        // while (i < 5) {
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        // // do while loop
+        // i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i < 5);
+
+        // // for-each loop
+        // String[] names = {"Alice", "Bob", "Charlie"};
+        // for (String name : names) {
+        //     System.out.println(name);
+        // }
+
+        // // break statement
+        // for (i = 0; i < 5; i++) {
+        //     if (i == 3) {
+        //         break; // exit the loop when i is 3
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // // continue statement
+        // for (i = 0; i < 5; i++) {
+        //     if (i == 3) {
+        //         continue; // skip the iteration when i is 3
+        //     }
+        //     System.out.println(i);
+        // }
+
+
+        // // using break and continue in nested loops
+        // outerLoop: for (int j = 0; j < 3; j++) {
+        //     for (i = 0; i < 5; i++) {
+        //         if (i == 3) {
+        //             break outerLoop; // exit both loops when i is 3
+        //         }
+        //         System.out.println("i: " + i + ", j: " + j);
+        //     }
+        // }
+
+
+        // infinite loop
+        // for(; ;) {
+        //     System.out.println("Apna College");
+        // }
+
+    }
+}
